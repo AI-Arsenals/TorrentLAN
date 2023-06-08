e7c4b4782fe603d6ba615761eefdf0664d5c73eb
Prakhar Gupta <gupta.99@iitj.ac.in>
2023-06-08 18:52:22 +0530

    added updation and removal in db_create.py

--- utils/client/db_create.py
+++ utils/client/db_create.py
@@ -4,9 +4,10 @@ import json
 import hashlib
 import time
 import platform
+import datetime
 
 SOFTWARE_DIR = os.getcwd()
-DATABASE_DIR = "./data/.db"
+DATABASE_DIR = "data/.db"
 DB_NAME = "file_tree.db"
 CONFIG_FOLDER_LOCATION = "configs/folder_locations.json"
 CONFIG_IDENTITY = "configs/identity.json"
@@ -59,6 +60,20 @@ def check_file_in_db(conn, lazy_file_hash, table_name):
 def create_connection():
     conn = sqlite3.connect(os.path.join(DATABASE_DIR, DB_NAME))
 
+    # # Delete Previous Tables
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';")
+    # tables = cursor.fetchall()
+    # for table in tables:
+    #     cursor.execute(f"DROP TABLE IF EXISTS {table[0]};")
+    # conn.commit()
+    # cursor.close()
+
+    return conn
+
+def delete_a_table(table_name):
+    conn=sqlite3.connect(os.path.join(DATABASE_DIR, DB_NAME))
+
     # Delete Previous Tables
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';")
@@ -105,9 +120,8 @@ def insert_item(conn, table_name, name, is_file, parent_id, child_id, metadata, 
     return last_row_id
 
 
-def insert_at_id(conn, table_name, insert_id, child_id):
+def update_child_at_id(conn, table_name, insert_id, child_id):
     child_id=', '.join(map(str, child_id))
-    print(f"Inserting {child_id} at {insert_id}")
     update_query = '''
         UPDATE {table_name}
         SET child_id = ?
@@ -119,6 +133,17 @@ def insert_at_id(conn, table_name, insert_id, child_id):
     conn.commit()
     cursor.close()
 
+def update_parent_at_id(conn, table_name, insert_id, parent_id):
+    update_query = '''
+        UPDATE {table_name}
+        SET parent_id = ?
+        WHERE id = ?;
+    '''
+    cursor = conn.cursor()
+    update_query = update_query.format(table_name=table_name)
+    cursor.execute(update_query, (parent_id, insert_id))
+    conn.commit()
+    cursor.close()
 
 def process_folder(conn, table_name, path, parent_id=None):
     if os.path.isfile(path):
@@ -134,6 +159,9 @@ def process_folder(conn, table_name, path, parent_id=None):
             # check if in db
             result = check_file_in_db(conn, lazy_file_hash, table_name)
             if len(result) > 0:
+                item_id = result[0][0]
+                update_parent_at_id(conn, table_name, item_id, parent_id)
+                child_ids.append(item_id)
                 continue
 
             hash_value = hash_generator(item_path)
@@ -141,15 +169,66 @@ def process_folder(conn, table_name, path, parent_id=None):
             child_ids.append(item_id)
         else:
             lazy_file_hash = lazy_file_check_hash(item_path, metadata['Last Modified'])
-            item_id = insert_item(conn, table_name, item, 0, parent_id, None, metadata, lazy_file_hash, UNIQUE_ID, "")
-            child_ids.append(item_id)
+            # check if in db
+            result = check_file_in_db(conn, lazy_file_hash, table_name)
+            if len(result) > 0:
+                item_id = result[0][0]
+                update_parent_at_id(conn, table_name, item_id, parent_id)
+                child_ids.append(item_id)
+            else:
+                item_id = insert_item(conn, table_name, item, 0, parent_id, None, metadata, lazy_file_hash, UNIQUE_ID, "")
+                child_ids.append(item_id)
             child_id_vals = process_folder(conn, table_name, item_path, item_id)
             if len(child_id_vals) > 0:
-                insert_at_id(conn, table_name, item_id, child_id_vals)
-    print(f"Child IDs: {child_ids}")
+                update_child_at_id(conn, table_name, item_id, child_id_vals)
     return child_ids
 
+def remove_deleted_files(conn, table_name, path):
+    IDS_TO_NOT_DELETE = []
 
+    cursor = conn.cursor()
+    def get_all_child_ids(conn, table_name, id):
+        cursor.execute(f"SELECT child_id FROM {table_name} WHERE id = ?;", (id,))
+        child_ids = cursor.fetchall()
+        if len(child_ids) == 0:
+            return
+        child_ids=child_ids[0][0]
+        if(child_ids is None):
+            return
+        child_ids=[int(x.strip()) for x in child_ids.split(",")]
+
+        for child_id in child_ids:
+            IDS_TO_NOT_DELETE.append(child_id)
+            get_all_child_ids(conn, table_name, child_id)
+
+    # ROOT NODE
+    cursor.execute(f"SELECT id,child_id FROM {table_name} WHERE parent_id IS NULL;")
+    root_ids = cursor.fetchall()
+    IDS_TO_NOT_DELETE.append(root_ids[0][0])
+    root_ids=root_ids[0][1]
+    if(root_ids is None):
+        return
+    root_ids=[int(x.strip()) for x in root_ids.split(",")]
+    
+    for root_id in root_ids:
+        IDS_TO_NOT_DELETE.append(root_id)
+        get_all_child_ids(conn, table_name, root_id)
+
+    # Remove all IDs not present in 'IDS_TO_NOT_DELETE'
+    cursor.execute(f"SELECT id FROM {table_name};")
+    all_ids = cursor.fetchall()
+    all_ids = [x[0] for x in all_ids]
+    cursor.close()
+
+    ids_to_delete = list(set(all_ids) - set(IDS_TO_NOT_DELETE))
+
+    cursor = conn.cursor()
+    if ids_to_delete:
+        for id in ids_to_delete:
+            cursor.execute(f"DELETE FROM {table_name} WHERE id = ?;", (id,))
+    cursor.close()
+
+    conn.commit()
 
 
 def main():
@@ -158,16 +237,35 @@ def main():
     
     conn = create_connection()
     for key, value in data.items():
-        path=value
-        name=key
+        update_date = datetime.datetime.strptime(value["update_date"], "%Y-%m-%d %H:%M:%S.%f")
+        current_time = datetime.datetime.now()
+        elapsed_time = current_time - update_date
+        if elapsed_time.total_seconds() >= 24 * 60 * 60 or False: 
+            continue
+        path = value["path"]
+        name = key
+        data[key]["update_date"] = str(datetime.datetime.now())
+        # delete_a_table(name)
         create_table(conn, name)
 
         # root folder
-        item_id=insert_item(conn, name, "root", 0, None, None, {}, "", UNIQUE_ID, "")
-        child_id_vals=process_folder(conn, name, path)
-        insert_at_id(conn, name, item_id, child_id_vals)
+        metadata=get_metadata(path)
+        lazy_file_hash = lazy_file_check_hash(path, metadata['Last Modified'])
+        # check if in db
+        result = check_file_in_db(conn, lazy_file_hash, name)
+        if len(result) > 0:
+            item_id = result[0][0]
+        else:
+            item_id=insert_item(conn, name, "root", 0, None, None, metadata, lazy_file_hash, UNIQUE_ID, "")
+        child_id_vals=process_folder(conn, name, path, item_id)
+        update_child_at_id(conn, name, item_id, child_id_vals)
+
+        # remove deleted files
+        remove_deleted_files(conn, name, path)
         
     conn.close()
+    with open(CONFIG_FOLDER_LOCATION, 'w') as f:
+        json.dump(data, f, indent=4)
 
 if __name__ == '__main__':
     main()
