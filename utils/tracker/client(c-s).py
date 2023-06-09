6c668a97ea816627c604c1c539d8dc24387022e1
Prakhar Gupta <gupta.99@iitj.ac.in>
2023-06-09 18:34:30 +0530

    Client-Server Model (OK TESTED)

--- utils/tracker/client(c-s).py
+++ utils/tracker/client(c-s).py
@@ -1,26 +1,29 @@
 import socket
 import os
 import json
-import subprocess
-import platform
+import base64
 
 CONFIG_IDENTITY = "configs/identity.json"
 CONFIG_CLIENT = "configs/client(c-s).json"
 OWN_UNIQUE_ID = json.load(open(CONFIG_IDENTITY))["client_id"]
-DBS_LOCATION = "./data/.db"+OWN_UNIQUE_ID
+DBS_LOCATION = "./data/.db/file_tree.db"
 
 PORT = 8293
 
-def update_server(unique_id,ip):
+def update_server(unique_id, ip):
     try:
         # Connect to server
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Connecting to server")
             s.connect((ip, PORT))
-            js_data={}
-            js_data[unique_id]=os.stat(DBS_LOCATION).st_mtime
-            with open(DBS_LOCATION,"rb") as f:
-                js_data["db_data"]=f.read()
-
+            print("Connected to server")
+            js_data = {}
+            js_data["unique_id"] = unique_id
+            with open(DBS_LOCATION, "rb") as f:
+                js_data["db_data"] = base64.b64encode(f.read()).decode()
+            data_to_send = json.dumps(js_data)
+            s.sendall(data_to_send.encode())
+            return True
     except ConnectionRefusedError:
         print("Server is down")
         return False
@@ -33,29 +36,31 @@ def get_ip_address(address):
         return None
 
 def check_updation(ip):
-    mtime=os.stat(DBS_LOCATION).st_mtime
+    mtime = os.stat(DBS_LOCATION).st_mtime
 
-    if not (os.path.exists(CONFIG_CLIENT)):
+    if not os.path.exists(CONFIG_CLIENT):
         with open(CONFIG_CLIENT, 'w') as f:
-            json.dump({"Last_Sent":mtime}, f)
-            success=update_server(OWN_UNIQUE_ID,ip)
-            if success:
-                with open(CONFIG_CLIENT, 'w') as f:
-                    json.dump({"Last_Sent":mtime}, f)
-            else:   
-                os.remove(CONFIG_CLIENT)
-
+            json.dump({"Last_Sent": mtime}, f)
+        success = update_server(OWN_UNIQUE_ID, ip)
+        if success:
+            print("Successfully updated server")
+            with open(CONFIG_CLIENT, 'w') as f:
+                json.dump({"Last_Sent": mtime}, f)
+        else:
+            os.unlink(CONFIG_CLIENT)
     else:
         with open(CONFIG_CLIENT, 'r') as f:
-            data=json.load(f)
-            if data["Last_Sent"]!=mtime:
-                success=update_server(OWN_UNIQUE_ID,ip)
+            data = json.load(f)
+            last_sent = data.get("Last_Sent",None)
+            if last_sent is None or last_sent != mtime:
+                success = update_server(OWN_UNIQUE_ID, ip)
                 if success:
+                    print("Successfully updated server")
                     with open(CONFIG_CLIENT, 'w') as f:
-                        json.dump({"Last_Sent":mtime}, f)
-
-
+                        json.dump({"Last_Sent": mtime}, f)
+            else:
+                print("Already Upto date")
 if __name__ == '__main__':
-    ip=get_ip_address('home.iitj.ac.in')
-    # print(ip)
+    ip = get_ip_address('home.iitj.ac.in')
+    print(f"Connecting to {ip}")
     check_updation(ip)
--- utils/tracker/server(c-s).py
+++ utils/tracker/server(c-s).py
@@ -1,8 +1,8 @@
-# Updated to work with python 2
 import socket
 import threading
 import os
 import json
+import base64
 
 HOST = ''
 PORT = 8293
@@ -10,28 +10,26 @@ PORT = 8293
 DB_LOCATION = "./data/.db"
 
 def handle_client(conn, addr):
-    print ("Connected by", addr)
+    print("Connected by", addr)
+    data=b""
     while True:
-        try:
-            data = conn.recv(1024)
-            if not data:
-                break
-
-            js_data=data.decode()
-            unique_id=js_data["unique_id"]
-            db_data=js_data["db_data"]
-            with open(os.path.join(DB_LOCATION,unique_id+".db"),"wb") as f:
-                f.write(db_data)
-        except socket.error:
-            print ("Connection with", addr, "closed")
+        chunk = conn.recv(1024)
+        if not chunk:
             break
+        data += chunk
+
+    js_data = json.loads(data.decode())
+    unique_id = js_data['unique_id']
+    db_data = base64.b64decode(js_data['db_data'])
+    with open(os.path.join(DB_LOCATION, unique_id + ".db"), "wb") as f:
+        f.write(db_data)
     conn.close()
 
 def start_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(250)
-    print ("Listening on", HOST + ":" + str(PORT) + "...")
+    print("Listening on", HOST + ":" + str(PORT) + "...")
     while True:
         conn, addr = s.accept()
 
