1324b7cce85ad13996865da338b9d3ceda3067a8
Prakhar Gupta <gupta.99@iitj.ac.in>
2023-06-14 13:02:50 +0530

    minor changes to client_ip_reg(c-s).py

---
+++ utils/tracker/client_ip_reg(c-s).py
@@ -0,0 +1,78 @@
+"""
+This module register the client ip with the pc ip in the server
+"""
+
+import socket
+import os
+import json
+import base64
+
+CONFIG_IDENTITY = "configs/identity.json"
+CONFIG_CLIENT = "configs/client_ip_reg(c-s).json"
+OWN_UNIQUE_ID = json.load(open(CONFIG_IDENTITY))["client_id"]
+
+PORT = 8888
+
+def update_server(unique_id, ip,local_conn_ip):
+    try:
+        # Connect to server
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Connecting to server")
+            s.connect((ip, PORT))
+            print("Connected to server")
+            js_data = {}
+            js_data["ip_reg"] = True
+            js_data["unique_id"] = unique_id
+            js_data["ip"] = local_conn_ip
+            data_to_send = json.dumps(js_data)
+            s.sendall(data_to_send.encode())
+            return True
+    except ConnectionRefusedError:
+        print("Server is down")
+        return False
+
+def get_ip_address(address):
+    try:
+        ip_address = socket.gethostbyname(address)
+        return ip_address
+    except socket.gaierror:
+        return None
+
+def get_my_connect_ip(conn_address):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((conn_address, 80))
+        local_conn_ip = s.getsockname()[0]
+        s.close()
+        return local_conn_ip
+    except socket.error:
+        return None
+    
+def update(ip):
+    local_conn_ip = get_my_connect_ip(ip)
+    if not os.path.exists(CONFIG_CLIENT):
+        with open(CONFIG_CLIENT, 'w') as f:
+            json.dump({"local_conn_ip": local_conn_ip}, f)
+        success = update_server(OWN_UNIQUE_ID, ip,local_conn_ip)
+        if success:
+            print("Successfully updated server")
+            with open(CONFIG_CLIENT, 'w') as f:
+                json.dump({"Last_Sent": local_conn_ip}, f)
+        else:
+            os.unlink(CONFIG_CLIENT)
+    else:
+        with open(CONFIG_CLIENT, 'r') as f:
+            data = json.load(f)
+            last_local_conn_ip = data.get("local_conn_ip",None)
+            if last_local_conn_ip is None or last_local_conn_ip != local_conn_ip:
+                success = update_server(OWN_UNIQUE_ID, ip,local_conn_ip)
+                if success:
+                    print("Successfully updated server")
+                    with open(CONFIG_CLIENT, 'w') as f:
+                        json.dump({"local_conn_ip": local_conn_ip}, f)
+            else:
+                print("Already Upto date")
+if __name__ == '__main__':
+    ip = get_ip_address('home.iitj.ac.in')
+    print(f"Connecting to {ip}")
+    update(ip)
