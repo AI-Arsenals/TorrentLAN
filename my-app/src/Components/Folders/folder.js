fa156f53c51e40a7780df47fa333dafb20f2a691
Sameer <ssjpr57@gmail.com>
2023-07-06 21:34:26 +0530

    removed backend folder

--- my-app/src/Components/Folders/folder.js
+++ my-app/src/Components/Folders/folder.js
@@ -7,59 +7,79 @@ import FolderItem from "./folder-item";
 
 const FolderView = (props) => {
   const [dir, setDir] = useState("root");
+  
   const [contentList, setContentList] = useState({ folders: [], files: [] });
   const [dir_text, setDirText] = useState("root");
-  const [downloadList,setDownloadList] = useState([])
-  
+  const [downloadList, setDownloadList] = useState([]);
+  const [currFolder, setCurrFolder] = useState([
+    -1,
+    "none",
+    0,
+    null,
+    null,
+    { Path:"root" ,Size: 89 },
+    null,
+    null,
+    null,
+  ]);
 
   const getFolderList = async () => {
-    let response = await fetch(`api/getEntries/${dir}`);
-    let data = await response.json();
-    setContentList(data);
-    // console.log(data)
-  };
-
-  
-
-  const deselectAll = ()=>{
-    setDownloadList([])
-    
-    var  list = document.querySelectorAll(".selected")
-    list.forEach(element=>{
-      element.classList.remove("selected")
-    })
-  }
-
-  const select=(name)=>{
-    
-    console.log(downloadList.find(e => e===name))
-    if(downloadList.find(e=> e===name)){
-
-      setDownloadList(prev=> prev.filter(e=> e!==name))
+    let response, data,depth;
+    if(currFolder[5].Path==null){
+      depth=2;
     }
     else{
 
-      setDownloadList(prev=>[...prev,name])
+      depth=currFolder[5].Path.split('//').length
     }
-    
-  }
 
+    if (depth ===1) {
+      response = await fetch(
+        `api/getFolderListAtDepth?depth=${depth}&folder=none`
+      );
+    }
+    else if(depth ===2){
+      response = await fetch(
+        `api/getFolderListAtDepth?depth=${depth}&folder=${currFolder[1]}`
+      );
+    }
+    else{
+      response = await fetch(
+        `api/getFolderList?unique_id=${currFolder[7]}&lazy_file_hash=${currFolder[6]}`
+      );
+    }
+    data = await response.json();
+    setContentList(data);
+    await setDir(currFolder[5].Path)
+  };
 
+  const deselectAll = () => {
+    setDownloadList([]);
 
-  const downloadHandler = () =>{
-    console.log(downloadList)
-  }
+    var list = document.querySelectorAll(".selected");
+    list.forEach((element) => {
+      element.classList.remove("selected");
+    });
+  };
 
- 
+  const select = (name) => {
+    console.log(downloadList.find((e) => e === name));
+    if (downloadList.find((e) => e === name)) {
+      setDownloadList((prev) => prev.filter((e) => e !== name));
+    } else {
+      setDownloadList((prev) => [...prev, name]);
+    }
+  };
+
+  const downloadHandler = () => {
+    console.log(downloadList);
+  };
 
   useEffect(() => {
     getFolderList();
-    setDirText(() => {
-      return dir.replaceAll("%", "/");
-    });
-    deselectAll()
     
-  }, [dir]);
+    deselectAll();
+  }, [currFolder]);
 
   const backButtonHandler = () => {
     setDir((data) => {
@@ -71,50 +91,49 @@ const FolderView = (props) => {
     });
   };
 
-
-
   return (
     <div className="main-container">
       <div className="viewport">
         <div className="navbar">
           <div className="left-content">
-
             <i
               className="fa-solid fa-arrow-left fa-2xl"
               onClick={backButtonHandler}
             ></i>
-            <div className="dir-text">{dir_text}</div>
+            <div className="dir-text">{dir}</div>
           </div>
 
           <div className="right-content">
-            {downloadList.length>0?
-          <i className="fa-regular fa-circle-down fa-2xl" onClick={downloadHandler}></i>: <div></div>}
+            {downloadList.length > 0 ? (
+              <i
+                className="fa-regular fa-circle-down fa-2xl"
+                onClick={downloadHandler}
+              ></i>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
 
-        
-          {contentList["folders"].map((name, index) => (
-            <FolderItem
-              key={index}
-              name={name}
-              type="folder"
-              handleClick={() => setDir((dir) => (dir += "%" + name))}
-              handleRightClick={()=> select(name)}
-              
-            />
-          ))}
+        {contentList["folders"].map((item, index) => (
+          <FolderItem
+            key={index}
+            name={item[1]}
+            type="folder"
+            handleClick={async ()=>{await setCurrFolder(item)}}
+            handleRightClick={() => select(item[1])}
+          />
+        ))}
 
-          {contentList["files"].map((name, index) => (
-            <FolderItem
-              key={index}
-              name={name}
-              type="file"
-              
-              handleRightClick={()=>select(name)}            
-              handleClick={() => 1}
-            />
-          ))}
-        
+        {contentList["files"].map((item, index) => (
+          <FolderItem
+            key={index}
+            name={item[1]}
+            type="file"
+            handleRightClick={() => select(item[1])}
+            handleClick={() => 1}
+          />
+        ))}
       </div>
     </div>
   );
