29b48c832be41131e78da09eb93f47ea3916525d
Prakhar Gupta <95362168+prakharguptaujjain@users.noreply.github.com>
2023-07-21 13:01:00 +0530

    added logging module in log

--- utils/log/main.py
+++ utils/log/main.py
@@ -1,10 +1,11 @@
 import datetime
 import os
 import json
+import logging
 import sys
 from termcolor import colored, cprint
 
-CONFIG_FILE="configs/log_config.json"
+CONFIG_FILE = "configs/log_config.json"
 
 class DebugModule:
     def __init__(self, file_name='log.txt'):
@@ -12,25 +13,45 @@ class DebugModule:
         LOGS_FILE_PATH = json.load(open(CONFIG_FILE))["logs_file_path"]
         self.file_path = os.path.join(LOGS_FILE_PATH, file_name)
         if not os.path.exists(self.file_path):
-            with open(self.file_path, "w") as f:
-                f.write("")
+            with open(self.file_path, "w"):
+                pass
+
+        self.logger = logging.getLogger('debug_logger')
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            '[%(asctime)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+        file_handler = logging.FileHandler(self.file_path)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
 
     def log_message(self, message, severity_no=0, script_name=None):
         severities = {0: "INFO", 1: "WARNING", 2: "ERROR"}
-        severity = severities.get(severity_no, "info")
+        severity = severities.get(severity_no, "INFO")
 
         color_map = {0: 'green', 1: 'yellow', 2: 'red'}
         color = color_map.get(severity_no, 'white')
 
         try:
             module_name = script_name
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_message = f"[{current_time}] [{module_name}] [{severity.upper()}] {message}\n"
+            log_message = f"[{module_name}] [{severity}] {message}"
 
-            with open(self.file_path, 'a') as file:
-                file.write(log_message)
+            if severity_no == 0:
+                self.logger.info(log_message)
+            elif severity_no == 1:
+                self.logger.warning(log_message)
+            elif severity_no == 2:
+                self.logger.error(log_message)
+            else :
+                # unknown severity
+                self.logger
 
-            print(colored(f"[{severity.upper()}] {message}", color))
+            print(colored(log_message, color))
         except IOError:
             print(colored(f"Error: Unable to write to file: {self.file_path}", 'red'))
 
@@ -53,9 +74,6 @@ def log(msg, severity_no=0, *args,file_name='log.txt'):
         
     logger.log_message(msg, severity_no, calling_script_name)
 
-
-
-
 def show_logs():
     logger=DebugModule("log.txt")
     logger.show_logs()
@@ -69,8 +87,8 @@ def log_location_set():
     if not os.path.exists(LOGS_FILE_PATH):
         os.makedirs(LOGS_FILE_PATH)
     if not os.path.exists(os.path.join(LOGS_FILE_PATH, "log.txt")):
-        with open(os.path.join(LOGS_FILE_PATH, "log.txt"), "w") as f:
-            f.write("")
+        with open(os.path.join(LOGS_FILE_PATH, "log.txt"), "w"):
+            pass
 
 if __name__ == '__main__':
     log("testing",1)
